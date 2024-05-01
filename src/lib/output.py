66561afb2571410051e2ef49c6940cfa77d81dd9
Lowkey3838 <public3838@bahnhof.se>
2024-05-01 16:09:01 +0200

    Finnished move of code from main to lib module

--- src/lib/output.py
+++ src/lib/output.py
@@ -1,19 +1,18 @@
 from functools import partial
 import shutil
 from colorama import Fore
-
-# from colorama import Fore, Back, Style
-from argparse import ArgumentParser
+import typing
 
 
 class formated_print:
 
     def __init__(self, mono: bool = False, quiet: bool = False):
         columns, rows = shutil.get_terminal_size()
-        self.rows = int(rows)
-        self.columns = int(columns)
-        self.quiet = quiet
-        self.mono = mono
+        self.rows: int = int(rows)
+        self.columns: int = int(columns)
+        self.quiet: bool = quiet
+        self.mono: bool = mono
+
         error = partial(
             self.print_formated_string, colour=self.red, mono=mono, quiet=quiet
         )
@@ -22,18 +21,25 @@ class formated_print:
             self.print_formated_string, colour=self.yellow, mono=mono, quiet=quiet
         )
         setattr(self, "warning", warning)
-        warning = partial(
-            self.print_formated_string, colour=self.yellow, mono=mono, quiet=quiet
+        normal = partial(
+            self.print_formated_string, colour=self.green, mono=mono, quiet=quiet
         )
-        setattr(self, "warning", warning)
+        setattr(self, "normal", normal)
 
-    def error(self, *args, **kvargs): ...
-    def warning(self, *args, **kvargs): ...
+    # These placeholder methods help IDE to find methods actually created in __init__
+    # type: ignore
+    def error(self, line: str): ...  # type: ignore
+    def warning(self, line: str): ...
+    def normal(self, line: str): ...
 
     def print_formated_string(
-        self, line: str, colour: callable, mono: bool, quiet: bool
+        self, line: str, colour: typing.Callable, mono: bool, quiet: bool
     ):
-        self.p(colour(line))
+        if not quiet:
+            if mono:
+                self.p(line)
+            else:
+                self.p(colour(line))
 
     def p(self, line):
         """Print line unless line empty"""
@@ -52,33 +58,33 @@ class formated_print:
     """ colour control """
 
     def blue(self, line):
-        return self.monochrome(Fore.BLUE, line)
+        return Fore.BLUE + line + Fore.RESET
 
     def red(self, line):
-        return self.monochrome(Fore.RED, line)
+        return Fore.RED + line + Fore.RESET
 
     def green(self, line):
-        return self.monochrome(Fore.GREEN, line)
+        return Fore.GREEN + line + Fore.RESET
 
     def yellow(self, line):
-        return self.monochrome(Fore.YELLOW, line)
+        return Fore.YELLOW + line + Fore.RESET
 
     def magenta(self, line):
-        return self.monochrome(Fore.MAGENTA, line)
+        return Fore.MAGENTA + line + Fore.RESET
 
     def cyan(self, line):
-        return self.monochrome(Fore.CYAN, line)
+        return Fore.CYAN + line + Fore.RESET
 
     def reset(self, line):
-        return self.monochrome(Fore.RESET, line)
+        return Fore.RESET + line
 
-    def quiet(self, line):
-        """if quiet return empty string"""
-        if args.quiet:
-            return ""
-        else:
-            return line
-
+    # def quiet(self, line):
+    #     """if quiet return empty string"""
+    #     if args.quiet:
+    #         return ""
+    #     else:
+    #         return line
+    #
     def monochrome(self, c, line):
         """if monochrome return clean string"""
         if self.mono:
@@ -94,14 +100,18 @@ class formated_print:
             template = "{:>" + str(size) + "} " + div + " "
         return template.format(label)
 
-    def col(self, l, label_size, div):
+    def col(self, lable, label_size, div):
         """format column string"""
-        return self.label(l, label_size, "l", div)
+        return self.label(lable, label_size, "l", div)
 
-    def print_line(pself, roperty, property_size, value, i):
+    def print_line(self, property, property_size, value, i):
         """present property line"""
         tmpl = "{:<" + "{}".format(str(property_size)) + "} : "
-        self.p(" " * i + green(tmpl.format(property.upper())) + yellow(str(value)))
+        self.p(
+            " " * i
+            + self.green(tmpl.format(property.upper()))
+            + self.yellow(str(value))
+        )
 
 
 if __name__ == "__main__":
@@ -117,4 +127,12 @@ if __name__ == "__main__":
     b = formated_print(mono=True)
     b.warning("Test warning")
     c = formated_print(quiet=True)
-    c.warning("Test warning")
+
+    e = formated_print()
+    e.normal("Test normal")
+    f = formated_print(mono=True)
+    f.normal("Test normal")
+    g = formated_print(quiet=True)
+    g.normal("Test normal")
+
+    a.print_line("SSIZE", 25, "2222", 5)

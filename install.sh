52d78e073fcd67fbd9dd2f4e42c846187c3f35dc
Lowkey3838 <public3838@bahnhof.se>
2024-05-07 03:06:47 +0200

    Fixed linting issues

--- install.sh
+++ install.sh
@@ -17,6 +17,10 @@ if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$NAME
     VER=$VERSION_ID
+elif [ -f /etc/fedora-release ]; then
+    # Older Fedora ...
+    OS=Fedora
+    VER=$(cat /etc/fedora-release)
 elif type lsb_release >/dev/null 2>&1; then
     # linuxbase.org
     OS=$(lsb_release -si)
@@ -30,12 +34,12 @@ elif [ -f /etc/debian_version ]; then
     # Older Debian/Ubuntu/etc.
     OS=Debian
     VER=$(cat /etc/debian_version)
-elif [ -f /etc/SuSe-release ]; then
-    # Older SuSE/etc.
-    ...
-elif [ -f /etc/redhat-release ]; then
-    # Older Red Hat, CentOS, etc.
-    ...
+# elif [ -f /etc/SuSe-release ]; then
+#     # Older SuSE/etc.
+#     ...
+# elif [ -f /etc/redhat-release ]; then
+#     # Older Red Hat, CentOS, etc.
+#     ...
 else
     # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
     OS=$(uname -s)
@@ -43,8 +47,8 @@ else
 fi
 
 # Inform user
-echo $OS
-echo $VER
+echo "$OS"
+echo "$VER"
 echo "Installing lsusbblk"
 
 # Install dependencies
